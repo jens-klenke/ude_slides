14e57e27a91a33b6f3726dfc26672504ee0bd7df
Klenke <jens.klenke@wiwinf.uni-due.de>
2020-10-21 13:59:19 +0200

    set_up

---
+++ 00_packages/packages.R
@@ -0,0 +1,13 @@
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(dplyr,
+               ggplot2,
+               here,
+               purrr,
+               magick,
+               stargazer,
+               dplyr,
+               tidyr,
+               papeR,
+               kableExtra,
+               gridExtra,
+               cowplot)
