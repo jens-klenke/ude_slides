81c1c002131def5c6f076685e9e621f054d50164
jens-klenke <57100601+jens-klenke@users.noreply.github.com>
2020-10-22 10:41:36 +0200

    update

--- 00_packages/packages.R
+++ 00_packages/packages.R
@@ -1,6 +1,7 @@
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(dplyr,
                ggplot2,
+               RefManageR,
                here,
                purrr,
                magick,
