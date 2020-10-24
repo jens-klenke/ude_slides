ca55015ef429ad1b8d8916a47bd03e1414e9aa1c
jens-klenke <57100601+jens-klenke@users.noreply.github.com>
2020-10-24 21:38:21 +0200

    Update printing.R

--- 01_slides/printing.R
+++ 01_slides/printing.R
@@ -7,4 +7,7 @@ pagedown::chrome_print(here::here("01_slides/vorkurs_1.Rmd"))
 # 2 
 pagedown::chrome_print(here::here("01_slides/vorkurs_2.Rmd"))
 
+# 3
+pagedown::chrome_print(here::here("01_slides/vorkurs_3.Rmd"))
+
 
